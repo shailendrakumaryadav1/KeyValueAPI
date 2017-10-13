d8e5695f40d510a1d9e1f57f2407a906026b1252
shailendra <shailendra@healint.com>
2017-10-13 19:05:08 +0800

    KeyValueController.js file created

---
+++ api/controllers/keyValueController.js
@@ -0,0 +1,109 @@
+'use strict';
+
+
+var mongoose = require('mongoose'),
+  KeyTimeValues = mongoose.model('KeyTimeValues'),
+  Value = mongoose.model('Value'),
+  Error_Message = mongoose.model('Error_Message');
+
+
+console.log("Controller executed!");
+
+
+exports.create_a_key_value_pair = function (req, res) {
+  console.log(req.body);
+
+  var key, value, timestamp = Date.now();
+  timestamp = (timestamp - timestamp % 1000) / 1000;
+  var jsonBody = req.body;
+  for (var k in jsonBody) {
+    if (jsonBody.hasOwnProperty(k)) {
+      key = k;
+      value = jsonBody[k];
+    }
+  }
+
+  console.log('key is = ' + key);
+  console.log('value is =' + value);
+
+  var key_value_item = new KeyTimeValues({
+    key: key,
+    value: value,
+    timestamp: timestamp
+  })
+
+  console.log('made key value item is = ' + key_value_item);
+
+  key_value_item.save(function (err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
+
+
+
+exports.read_a_key = function (req, res) {
+
+  console.log('Reading a key');
+
+  var key = req.params.key,
+    timestamp = req.query.timestamp;
+
+
+  console.log('Requested key is ' + key);
+
+
+
+  if (timestamp)
+    console.log('Timestamp is ' + timestamp);
+  else {
+    timestamp = Date.now();
+    timestamp = (timestamp - timestamp % 1000) / 1000;
+    console.log('Timestamp is absent, use now timestamp = ' + timestamp);
+
+  }
+
+
+  KeyTimeValues.find({ "key": key }, function (err, result) {
+
+    if (err)
+      res.send(err);
+
+
+    if (result.length == 0) {
+
+      // return that no key exist in db
+      var error_msg = new Error_Message({
+        message: 'Key does not exist'
+      });
+      res.status(404).send(error_msg);
+    }
+    else {
+      var max = -1, index = -1;
+
+      for (var i = 0; i < result.length; i++) {
+        var item = result[i];
+        if (item.timestamp > max && item.timestamp <= timestamp) {
+          max = item.timestamp;
+          index = i;
+        }
+      }
+
+      if (index == -1) {
+        // key does not exist at this time.
+        var error_msg = new Error_Message({
+          message: 'Key does not exist at this timestamp'
+        });
+        res.status(404).send(error_msg);
+      }
+      else {
+        // key value is at index;
+        var v = new Value({
+          value: result[index].value
+        });
+        res.json(v);
+      }
+    }
+  });
+};
