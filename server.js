455ea2ea404f8f3cb8f156e10a1d6724000f9af9
shailendra <shailendra@healint.com>
2017-10-13 19:01:35 +0800

    server.js file updated

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 8080;
+  port = process.env.PORT || 8080,
+  mongoose = require('mongoose'),
+  Task = require('./api/models/keyValueModel'), //created model loading here
+  bodyParser = require('body-parser');
+
+
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/key-value-db'); 
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+// app.use(function(req, res) {
+//   res.status(404).send({url: req.originalUrl + ' not found'})
+// });
+
+
+var routes = require('./api/routes/keyValueRoutes'); //importing route
+routes(app); //register the route
 
 app.listen(port);
 
