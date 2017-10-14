a13005777e20438bca4458ccbfa3822f7c4c9292
shailendra <shailendra@healint.com>
2017-10-15 02:12:53 +0800

    Test cases added for GET and POST requests

--- test/test.js
+++ test/test.js
@@ -9,31 +9,199 @@ let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../server');
 let should = chai.should();
+let KeyTimeValues = mongoose.model('KeyTimeValues');
 
 chai.use(chaiHttp);
 //Our parent block
 describe('Models', () => {
     beforeEach((done) => { //Before each test we empty the database
-        console.log('Before executed.');
-        models.remove({}, (err) => {
+        KeyTimeValues.remove({}, (err) => {
+
+            var key_value1_item = new KeyTimeValues({
+                key: 'Key_1',
+                value: 'Value_at_10000',
+                timestamp: 10000
+            });
+
+            var key_value2_item = new KeyTimeValues({
+                key: 'Key_1',
+                value: 'Value_at_20000',
+                timestamp: 20000
+            });
+
+            key_value1_item.save(function (err, result) { });
+            key_value2_item.save(function (err, result) { });
+
             done();
         });
     });
+
     /*
       * Test the /GET route
       */
-    describe('/GET object/Key_K', () => {
-        it('it should GET the value for key = Key_K', (done) => {
+    describe('/GET object/Key_not_exist', () => {
+        it('it should GET the value for key = Key_not_exist when key does not exist at any time.', (done) => {
             chai.request(server)
-                .get('/object/Key_K')
+                .get('/object/Key_not_exist')
                 .end((err, res) => {
-                    console.log('inside end block');
                     res.should.have.status(404);
-                    //res.body.should.be.a('array');
-                    //res.body.length.should.be.eql(0);
+                    res.body.should.be.a('object');
+                    Object.keys(res.body).length.should.equal(1);
+                    res.body.should.have.property("message");
+                    res.body["message"].should.equal("Key does not exist");
                     done();
                 });
         });
     });
 
+    describe('/GET object/Key_1?timestamp=9999', () => {
+        it('it should GET the value for key = Key_1 when key exist but not at that timestamp', (done) => {
+            chai.request(server)
+                .get('/object/Key_1?timestamp=9999')
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    res.body.should.be.a('object');
+                    Object.keys(res.body).length.should.equal(1);
+                    res.body.should.have.property("message");
+                    res.body["message"].should.equal("Key does not exist at this timestamp");
+                    done();
+                });
+        });
+    });
+
+    describe('/GET object/Key_1?timestamp=10000', () => {
+        it('it should GET the old value for key = Key_1 since timestamp is 10000', (done) => {
+            chai.request(server)
+                .get('/object/Key_1?timestamp=10000')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    Object.keys(res.body).length.should.equal(1);
+                    res.body.should.have.property("value");
+                    res.body["value"].should.equal("Value_at_10000");
+                    done();
+                });
+        });
+    });
+
+    describe('/GET object/Key_1?timestamp=10001', () => {
+        it('it should GET the old value for key = Key_1 since timestamp is 15000', (done) => {
+            chai.request(server)
+                .get('/object/Key_1?timestamp=10001')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    Object.keys(res.body).length.should.equal(1);
+                    res.body.should.have.property("value");
+                    res.body["value"].should.equal("Value_at_10000");
+                    done();
+                });
+        });
+    });
+
+    describe('/GET object/Key_1', () => {
+        it('it should GET the latest value for key = Key_1', (done) => {
+            chai.request(server)
+                .get('/object/Key_1')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    Object.keys(res.body).length.should.equal(1);
+                    res.body.should.have.property("value");
+                    res.body["value"].should.equal("Value_at_20000");
+                    done();
+                });
+        });
+    });
+
+    describe('/GET object/Key_1?timestamp=20000', () => {
+        it('it should GET the new value for key = Key_1 since timestamp is 20000', (done) => {
+            chai.request(server)
+                .get('/object/Key_1?timestamp=20000')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    Object.keys(res.body).length.should.equal(1);
+                    res.body.should.have.property("value");
+                    res.body["value"].should.equal("Value_at_20000");
+                    done();
+                });
+        });
+    });
+
+    describe('/GET object/Key_1?timestamp=19999', () => {
+        it('it should GET the old value for key = Key_1 since timestamp is 19999', (done) => {
+            chai.request(server)
+                .get('/object/Key_1?timestamp=19999')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    Object.keys(res.body).length.should.equal(1);
+                    res.body.should.have.property("value");
+                    res.body["value"].should.equal("Value_at_10000");
+                    done();
+                });
+        });
+    });
+
+
+    /*
+      * Test the /POST route
+      */
+    describe('/POST object', () => {
+        it('it should not POST an object without any request body', (done) => {
+            let pair = null;
+            chai.request(server)
+                .post('/object')
+                .send(pair)
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    res.body.should.be.a('object');
+                    Object.keys(res.body).length.should.equal(1);
+                    res.body.should.have.property('message').eql('Key-Value not in proper format');
+                    done();
+                });
+        });
+
+        it('it should not POST an object with multiple key-value pair in request body', (done) => {
+            let pair = {
+                key1: "value1",
+                key2: "value2"
+            }
+            chai.request(server)
+                .post('/object')
+                .send(pair)
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    res.body.should.be.a('object');
+                    Object.keys(res.body).length.should.equal(1);
+                    res.body.should.have.property('message').eql('Key-Value not in proper format');
+                    done();
+                });
+        });
+
+        it('it should POST an object with a key-value pair in request body', (done) => {
+            let pair = {
+                key1: "value1"
+            }
+            chai.request(server)
+                .post('/object')
+                .send(pair)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    Object.keys(res.body).length.should.equal(3);
+                    res.body.should.have.property('key').eql('key1');
+                    res.body.should.have.property('value').eql('value1');
+
+                    KeyTimeValues.find({ key: "key1", value: "value1" },
+                        function (err, result) {
+                            result.length.should.equal(1);
+                            res.body.should.have.property('timestamp').eql(result[0].timestamp);
+                        })
+                    done();
+                });
+        });
+
+    });
 });
